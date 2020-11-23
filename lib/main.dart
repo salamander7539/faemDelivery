b09ba34ef30e2254ab9aa720d3ab8d5ef949b6df
aytoku <seturidee@mail.ru>
2020-11-23 14:51:01 +0300

    IOs Build

--- lib/main.dart
+++ lib/main.dart
@@ -13,6 +13,7 @@ import 'package:faem_delivery/deliveryJson/switch_deliver_status.dart';
 import 'package:faem_delivery/taxi_menu.dart';
 import 'package:faem_delivery/tokenData/refresh_token.dart';
 import 'package:flutter/material.dart';
+import 'package:fluttertoast/fluttertoast.dart';
 import 'package:geolocator/geolocator.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
